69355d489963e37a8adca24fe2548a8d500762fc
Your Namegit config --global user.name Veronika <veronika.tarasova.98@gmail.com>
2022-10-27 13:10:24 +0300

    task completed

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -4,6 +4,16 @@ import java.util.OptionalInt;
 
 public class MaxMethod {
     public static OptionalInt max(int[] values) {
-        throw new UnsupportedOperationException();
+        OptionalInt maxOptional = OptionalInt.empty();
+        if(values != null && values.length != 0) {
+            int maxValue = values[0];
+            for (int value : values) {
+                if (maxValue < value) {
+                    maxValue = value;
+                }
+                maxOptional = OptionalInt.of(maxValue);
+            }
+        }
+        return maxOptional;
     }
 }
